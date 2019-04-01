1cc9228cf0bc2fb74d4165a312b3682766ab202b
BryanGI27 <bryan.shw27@gmail.com>
2019-04-01 07:55:18 -0600

    Initial commit

---
+++ ProyectoFinalEquipo2Tests/ProyectoFinalEquipo2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ProyectoFinalEquipo2Tests.swift
+//  ProyectoFinalEquipo2Tests
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 19-03-26.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import XCTest
+@testable import ProyectoFinalEquipo2
+
+class ProyectoFinalEquipo2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ProyectoFinalEquipo2
